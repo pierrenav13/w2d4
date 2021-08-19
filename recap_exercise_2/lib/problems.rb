56559cb43b2635bece64634735d0a005dae66e9f
pierre <pnavarin@gmail.com>
2021-08-19 14:18:18 -0400

    Finished Recap 2

--- recap_exercise_2/lib/problems.rb
+++ recap_exercise_2/lib/problems.rb
@@ -1,21 +1,38 @@
 # Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
 # of both of the given numbers
 def least_common_multiple(num_1, num_2)
-
+    max = num_1 * num_2
+    (2..max).each {|multiple| return multiple if multiple % num_1 == 0 && multiple % num_2 == 0}
 end
 
 
 # Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
 # most in the string.
 def most_frequent_bigram(str)
+    x = 0
+    y = 1
+    bigrams = []
+    while y < str.length
+        bigrams << str[x..y]
+        x += 1
+        y += 1
+    end
+    hash = Hash.new(0)
+    bigrams.each do |bigram|
+        hash[bigram] += 1 if str.include?(bigram)
+    end
 
+    i = hash.values.max
+    hash.key(i)
 end
 
 
 class Hash
     # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
     def inverse
-
+        new_hash = {}
+        self.each {|k,v| new_hash[v] = k}
+        new_hash
     end
 end
 
@@ -23,7 +40,15 @@ end
 class Array
     # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
     def pair_sum_count(num)
-
+        count = 0
+        self.each_with_index do |ele1, idx1|
+            self.each_with_index do |ele2, idx2|
+                if idx1 < idx2
+                    count += 1 if ele1 + ele2 == num
+                end
+            end
+        end
+        count
     end
 
     # Write a method, Array#bubble_sort, that takes in an optional proc argument.
@@ -40,6 +65,34 @@ class Array
     #
     # This should remind you of the spaceship operator! Convenient :)
     def bubble_sort(&prc)
+        solved = false
+        l = self.length - 1
+        
 
+        while solved == false
+            solved = true
+            i = 0
+            if prc == nil
+                while i < l
+                    if self[i] > self[i + 1]
+                        solved = false
+                        self[i], self[i + 1] = self[i + 1], self[i]
+                    end
+                    i += 1
+                end
+
+            else
+                while i < l
+                    n = prc.call(self[i], self[i + 1])
+                    if n == 1
+                        solved = false
+                        self[i], self[i + 1] = self[i + 1], self[i]
+                    end
+                    i += 1
+                end
+            end
+            
+        end
+        self
     end
 end
